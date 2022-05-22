8e3768a127a9f2d5393235766bda5a60b71c2655
Philpascal <96484266+Philpascal@users.noreply.github.com>
2022-05-22 16:51:48 +0200

    modale

---
+++ public/facteuraccordeur/main.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("modal")
+
+function openModal(){
+    modal.style.top="90px"
+}
+
+function closeModal(){
+    modal.style.top="-500px"
+    
+}
--- src/Controller/AccordController.php
+++ src/Controller/AccordController.php
@@ -9,9 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccordController extends AbstractController
 {
     #[Route('/accord', name: 'app_accord')]
-    public function index(): Response
+    public function indexaccord(): Response
     {
         return $this->render('accord/index.html.twig', [
+            'controller_name' => 'AccordController',
+        ]);
+    }
+
+    #[Route('/reparation', name: 'app_reparation')]
+    public function indexreparation(): Response
+    {
+        return $this->render('reparation/index.html.twig', [
+            'controller_name' => 'AccordController',
+        ]);
+    }
+
+    #[Route('/conseil', name: 'app_conseil')]
+    public function indexconseil(): Response
+    {
+        return $this->render('conseil/index.html.twig', [
+            'controller_name' => 'AccordController',
+        ]);
+    }
+
+    #[Route('/urgence', name: 'app_urgence')]
+    public function indexurgence(): Response
+    {
+        return $this->render('urgence/index.html.twig', [
+            'controller_name' => 'AccordController',
+        ]);
+    }
+
+    #[Route('/zone', name: 'app_zone')]
+    public function indexzone(): Response
+    {
+        return $this->render('zone/index.html.twig', [
             'controller_name' => 'AccordController',
         ]);
     }
